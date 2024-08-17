9071177fb0caf9b2c2d14c7d790fe040b86159e4
Ana Elisa Rodrigues do Couto <anaecouto@outlook.com>
2024-08-17 19:27:42 -0300

    removing unnecessary method

--- src/main/java/br/com/anaelisa/petproject/domain/validator/PasswordValidator.java
+++ src/main/java/br/com/anaelisa/petproject/domain/validator/PasswordValidator.java
@@ -8,10 +8,6 @@ public class PasswordValidator implements ConstraintValidator<ValidPassword, Str
     private static final String PASSWORD_PATTERN = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$";
 
     @Override
-    public void initialize(ValidPassword constraintAnnotation) {
-    }
-
-    @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
         if (password == null) {
             return false;
